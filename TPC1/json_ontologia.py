8844df7ebd3166c98d33cd2f386e7eddb93a32de
so.texera@hotmail.com <so.texera@hotmail.com>
2021-02-28 18:57:10 +0000

    tpc1

--- TPC1/json_ontologia.py
+++ TPC1/json_ontologia.py
@@ -3,15 +3,60 @@ import json
 with open("uni.json") as jsonfile:
     data = json.loads(jsonfile.read())
 
+f = open("uni.ttl", "w")
+
+f.write(''':ensina rdf:type owl:ObjectProperty ;
+        owl:inverseOf :ensinadaPor ;
+        rdfs:domain :Docente ;
+        rdfs:range :UnidadeCurricular .\n''')
+
+f.write(''':frequenta rdf:type owl:ObjectProperty ;
+           owl:inverseOf :frequentadaPor ;
+           rdfs:domain :Aluno ;
+           rdfs:range :UnidadeCurricular .\n''')
+
+f.write(''':alunoDe rdf:type owl:ObjectProperty ;
+          owl:inverseOf :professorDe .\n''')
+
+f.write(''':ensinadaPor rdf:type owl:ObjectProperty .\n''')
+f.write(''':frequentadaPor rdf:type owl:ObjectProperty .\n''')
+f.write(''':professorDe rdf:type owl:ObjectProperty ;
+              owl:propertyChainAxiom ( :ensina :frequentadaPor ) .\n''')
+
+f.write('\n')
+
+f.write(''':nome rdf:type owl:DatatypeProperty .\n''')
+f.write(''':idade rdf:type owl:DatatypeProperty .\n''')
+
+f.write('\n')
+
+f.write(''':Aluno rdf:type owl:Class ;
+       rdfs:subClassOf [ rdf:type owl:Restriction ;
+                         owl:onProperty :frequenta ;
+                         owl:someValuesFrom :UnidadeCurricular
+                       ] .\n''')
+f.write(''':Docente rdf:type owl:Class .\n''')
+f.write(''':UnidadeCurricular rdf:type owl:Class ;
+           rdfs:subClassOf [ rdf:type owl:Restriction ;
+                             owl:onProperty :ensinadaPor ;
+                             owl:someValuesFrom :Docente
+                           ] .\n''')
+
+f.write('\n')
+
 ucs = data[0]['ucs']
 for u in ucs:
-    str = ':' + u['codigo'] + ' rdf:type owl:NamedIndividual , :UnidadeCurricular ; :nome "' + u['nome'] + '"; :éEnsinadaPor :' + u['docente'] +' .'
-    print(str)
+    str = ':' + u['codigo'] + ' rdf:type owl:NamedIndividual , :UnidadeCurricular ; :nome "' + u['nome'] + '" ; :ensinadaPor :' + u['docente'] +' .\n'
+    f.write(str)
+
+f.write('\n')
 
 docentes = data[1]['docentes']
 for d in docentes:
-    str = ':' + d['nrDocente'] + ' rdf:type owl:NamedIndividual , :Docente ; :nome "' + d['nome'] + '" .'
-    print(str)
+    str = ':' + d['nrDocente'] + ' rdf:type owl:NamedIndividual , :Docente ; :nome "' + d['nome'] + '" .\n'
+    f.write(str)
+
+f.write('\n')
 
 alunos = data[2]['alunos']
 for a in alunos:
@@ -19,5 +64,7 @@ for a in alunos:
     str = ':' + a['nrAluno'] + ' rdf:type owl:NamedIndividual , :Aluno ; :nome "' + a['nome'] + f'" ; :idade "{idade}" ; :frequenta'
     for uc in a['ucs']:
         str += f' :{uc}'
-    str += ' .'
-    print(str)
+    str += ' .\n'
+    f.write(str)
+
+f.close()
